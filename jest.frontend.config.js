8eaa5835e507cae4a2f7cc56377f758e194ef177
Yu Heng Lee <89116915+Huggenguggen@users.noreply.github.com>
2025-02-27 16:36:07 +0800

    Add tests for context/search and pages/search
    Add tests for context/search.js
    Add tests for pages/Search.js
    Update jest.frontend.config.js to add those files to coverage and
    testing
    Fix minor bugs in context/search.js and pages/Search.js

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,19 @@ module.exports = {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
+  testMatch: [
+    "<rootDir>/client/src/pages/Auth/*.test.js",
+    "<rootDir>/client/src/pages/Search.test.js",
+    "<rootDir>/client/src/context/*.test.js",
+  ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: [
+    "client/src/pages/Auth/**",
+    "client/src/pages/Search.js",
+    "client/src/context/search.js",
+  ],
   coverageThreshold: {
     global: {
       lines: 80,
